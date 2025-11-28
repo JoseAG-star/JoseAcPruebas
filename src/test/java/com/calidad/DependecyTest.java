c54e51cfa78072c484f156b0c62428755eabd856
JoseAG-star <a18001336@alumnos.uady.mx>
2025-11-28 12:58:11 -0600

    Revisar dependecy

--- src/test/java/com/calidad/DependecyTest.java
+++ src/test/java/com/calidad/DependecyTest.java
@@ -29,13 +29,12 @@ dependency = new Dependency(subDependency);
 @Test
 void testSubDependencyClassName(){
     String esperado = "SubDependency.class";
-    when(subDependency.getClassName()).thenReturn(esperado);
     String resultadoEjecucion = dependency.getSubdependencyClassName();
     assertThat(resultadoEjecucion,is(esperado));
 }
 // Test: testSubDependencyClassNameMock
 // Este test simula (mock) la respuesta del método getClassName de la subdependencia.
-// Se espera que el mock retorne "SubDependency.class" forzadamentx 
+// Se espera que el mock retorne "SubDependency.class" forzadament
 @Test
 void testSubDependencyClassNameMock(){
     String esperado = "SubDependency.class";
