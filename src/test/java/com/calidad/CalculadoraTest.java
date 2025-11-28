369579b32e61cc5b086c9005d0162cce53f15560
EnriqueCD369 <chandiazenrique@gmail.com>
2025-11-27 20:45:33 -0600

    Se renombrarón clases para cumplir con la metrica, se movió la úbicación de schema

--- src/test/java/com/calidad/CalculadoraTest.java
+++ src/test/java/com/calidad/CalculadoraTest.java
@@ -1,81 +1,58 @@
 package com.calidad;
-import org.junit.jupiter.api.Test;
-import com.unittest.calculadora.calculadora;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import com.unittest.calculadora.Calculadora; 
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
 public class CalculadoraTest {
- public double operador1;
+    
+    public double operador1;
     public double operador2;
-    public calculadora calc;
+    public Calculadora calc; 
 
     @BeforeEach
-        void setip(){
-            operador1 = 10;
-            operador2 = 5;
-            calc = new calculadora();
-            System.out.println("Inicializando...");
-        }
+      @SuppressWarnings("unused")
+    void setup() { 
+        operador2 = 5;
+        calc = new Calculadora(); 
+        System.out.println("Inicializando...");
+    }
+
     @AfterEach
     public void cleanUp(){
         System.out.println("Prueba finalizada!");
     }
     
     @Test
-        void testSumaNumerosPositivos(){
-            //Inicializar datos
-            double resultadoEsperado = 15;
-
-            //Ejercitar el código
-            double resultado = calc.suma(operador1, operador2);
-
-            //Verificar
-            assertThat(resultadoEsperado, is(resultado));
-        }
-    @Test
-        void testRestaNumerosPositivos(){
-            //Inicializar datos
-            double resultadoEsperado = 5;
-
-            //Ejercitar el código
-            double resultado = calc.resta(operador1, operador2);
-
-            //Verificar
-            assertThat(resultadoEsperado, is(resultado));
-        }
-    @Test
-        void testMultiplicarNumerosPositivos(){
-            //Inicializar datos
-            double resultadoEsperado = 50;
-
-            //Ejercitar el código
-            double resultado = calc.multiplica(operador1, operador2);
-
-            //Verificar
-            assertThat(resultadoEsperado, is(resultado));
-        }
+    void testSumaNumerosPositivos(){
+        double resultadoEsperado = 15;
+        double resultado = calc.suma(operador1, operador2);
+        assertThat(resultadoEsperado, is(resultado));
+    }
 
     @Test
-        void testDivideNumerosPositivos(){
-            //Inicializar datos
-            double resultadoEsperado = 2;
+    void testRestaNumerosPositivos(){
+        double resultadoEsperado = 5;
+        double resultado = calc.resta(operador1, operador2);
+        assertThat(resultadoEsperado, is(resultado));
+    }
 
-            //Ejercitar el código
-            double resultado = calc.divide(operador1, operador2);
+    @Test
+    void testMultiplicarNumerosPositivos(){
+        double resultadoEsperado = 50;
+        double resultado = calc.multiplica(operador1, operador2);
+        assertThat(resultadoEsperado, is(resultado));
+    }
 
-            //Verificar
-            assertThat(resultadoEsperado, is(resultado));
-        }
-    /*@Test(expected = NullPointerException.class)    
-    public void whenExceptionThrown_thenExpectationSatisfied(){
-        String test = null;
-        test.length();
-    }*/
-
+    @Test
+    void testDivideNumerosPositivos(){
+        double resultadoEsperado = 2;
+        double resultado = calc.divide(operador1, operador2);
+        assertThat(resultadoEsperado, is(resultado));
+    }
 }
-
